c6f4015d4dd0f0a02c0b32f277e44b69a173d7f6
Diego Quispe <dquispe@domain.com.pe>
2025-12-26 13:13:20 -0500

    Agregando artifacts

--- src/train.py
+++ src/train.py
@@ -1,5 +1,23 @@
 import numpy as np
+import json 
+from pathlib import Path
 
-print("Startin traininig simulation...")
+print("Traininig model...")
+
 data= np.random.rand(100)
-print("Mean: ", data.mean())
+mean=data.mean()
+
+#Solo es una simulación
+
+model= {
+    "mean":float(mean),
+    "version":"v1"
+}
+
+Path("artifacts").mkdir(exist_ok=True)
+
+
+with open("artifacts/model.json","w") as f:
+    json.dump(model,f)
+
+print("Model saved!")
